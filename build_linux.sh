a64e9ee79b3dd472ca90ee7623317f7823acee6a
Marcelo Frau <marcelofrau@gmail.com>
2026-05-15 12:39:53 +0200

    Adding buildlinux script

--- build_linux.sh
+++ build_linux.sh
@@ -9,26 +9,13 @@ fi
 APP_NAME="gamelistify_v${APP_VERSION}"
 ICON_PATH="icons/app_icon.ico"
 
-uv add pyinstaller --dev
-
-PYTHON_BIN=$(uv run which python)
-PYTHON_HOME=$(uv run python -c "import sys; from pathlib import Path; print(Path(sys.executable).parent.parent)")
-LIB_PATH="${PYTHON_HOME}/lib"
-
-export LD_LIBRARY_PATH="$LIB_PATH:$LD_LIBRARY_PATH"
-
-# Localiza o diretório de bibliotecas real do Python 3.14
-PYTHON_LIB_DIR=$(uv run python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
-TCL_V="9.0"
-
-echo "Buscando libs em: ${PYTHON_LIB_DIR}"
-
-uv run pyinstaller --onefile --windowed \
-    --name="${APP_NAME}" \
-    --add-data "icons:icons" \
-    --collect-all tkinter \
-    --add-binary "${PYTHON_LIB_DIR}/libtcl${TCL_V}.so:." \
-    --add-binary "${PYTHON_LIB_DIR}/libtk${TCL_V}.so:." \
+uv add nuitka --dev
+uv run python -m nuitka \
+    --standalone \
+    --onefile \
+    --plugin-enable=tk-inter \
+    --include-data-dir=icons=icons \
+    --output-filename="${APP_NAME}" \
     main.py
 
 if [ $? -eq 0 ]; then
